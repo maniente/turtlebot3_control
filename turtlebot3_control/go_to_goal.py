591d5ac5c812f5cfef3a56d2fbbade8c2a382988
maniente <marcomusto@tutanota.com>
2024-10-29 17:25:27 +0200

    added theta pose in the final location

--- turtlebot3_control/go_to_goal.py
+++ turtlebot3_control/go_to_goal.py
@@ -32,35 +32,42 @@ class DriverNode(Node):
         self.pose.x = pose.position.x
         self.pose.y = pose.position.y
         self.pose.theta = math.atan2(siny_cosp, cosy_cosp)
+    
+    def angle_norm (self,angle):
         
+        while angle > math.pi:
+            angle -= 2 * math.pi
+        while angle < -math.pi:
+            angle += 2 * math.pi
+        
+        return angle
+
     def go_to_goal(self):
         goal = Pose()
         
         # Read goal positions from command-line arguments
         goal.x = float(sys.argv[1])
         goal.y = float(sys.argv[2])
-        
+        goal.theta = float(sys.argv[3])
+
         new_vel = Twist()
         
         # Print the difference to the goal as a debug statement
         distance_error = math.sqrt((goal.x - self.pose.x)**2 + (goal.y - self.pose.y)**2)
 
         angle_to_goal = math.atan2(goal.y - self.pose.y, goal.x - self.pose.x)
-        if abs(angle_to_goal) >(math.pi -0.05):
-            angle_to_goal 
+
         angle_error = angle_to_goal - self.pose.theta
-        
-        if angle_error > math.pi:
-            angle_error -= 2 * math.pi
-        if angle_error < -math.pi:
-            angle_error += 2 * math.pi
+        angle_error = self.angle_norm(angle_error)
+
+        pose_angle_err = goal.theta - self.pose.theta
+        pose_angle_err = self.angle_norm(pose_angle_err)
 
         self.get_logger().info(f'{angle_to_goal} - {self.pose.theta} ')
         self.get_logger().info(f'e: {distance_error}')
         
         self.get_logger().info(f'e: {angle_error}')
 
-        tolerance_angle = 0.5
         tolerance_dist = 0.1
 
         max_angular_vel = 3.0
@@ -70,17 +77,23 @@ class DriverNode(Node):
         if distance_error >= tolerance_dist:
             new_vel.angular.z = kp* angle_error
             new_vel.linear.x = kp * distance_error
-        else:
+            
+        elif abs(pose_angle_err) >= tolerance_dist:
             new_vel.linear.x = 0.0
+            new_vel.angular.z = kp*pose_angle_err
+            
+        else:
             new_vel.angular.z = 0.0
-            self.get_logger().info(f'Goal Reached')
+            new_vel.linear.x = 0.0
             self.cmd_vel_pub.publish(new_vel)
+            self.get_logger().info(f'Goal Reached')
             quit()
 
         if  new_vel.linear.x > max_val_x_vel:
             new_vel.linear.x = max_val_x_vel
         #new_vel.linear.x = 0.0
         self.cmd_vel_pub.publish(new_vel)
+        
         
     def stop_turtlebot(self):
         # define what happens when program is interrupted
